71fe337a39be16cfb622cd046877a65db84c4987
Brian <nbmbestre91@gmail.com>
2023-11-30 23:10:43 +0800

    Food component test update

--- src/components/FoodCard/FoodCard.test.tsx
+++ src/components/FoodCard/FoodCard.test.tsx
@@ -2,27 +2,14 @@ import { cleanup, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import FoodCard from './FoodCard';
 import { Food } from '../../lib/types/food';
-
-const data: Food = {
-	id: '628b5decee8163179ee23d3e',
-	index: 196,
-	rating: 4.2928,
-	promotion: '1+1',
-	isNew: false,
-	categoryId: '6288a89fe6c2fe0b758360fe',
-	minCookTime: 120,
-	maxCookTime: 140,
-	restaurant: 'Imkan',
-	name: 'Imkan Deserts',
-	imageUrl: 'https://source.unsplash.com/random/400x400?Deserts',
-};
+import { MOCK_FOODS } from '../../mocks/mockData';
 
 afterEach(() => {
 	cleanup();
 });
 
 describe('Food Card Component', () => {
-	render(<FoodCard food={data} />);
+	render(<FoodCard food={MOCK_FOODS.foods[0] as Food} />);
 	const food = screen.getByTestId('food');
 
 	test('Component rendered', () => {
--- src/components/FoodList/FoodList.test.tsx
+++ src/components/FoodList/FoodList.test.tsx
@@ -2,42 +2,14 @@ import { cleanup, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import FoodList from './FoodList';
 import { Food } from '../../lib/types/food';
-
-const data: Food[] = [
-	{
-		id: '628b5dec795c64a968193a0c',
-		index: 194,
-		rating: 3.984,
-		promotion: 'discount',
-		isNew: false,
-		categoryId: '6288a89fac9e970731bfaa7b',
-		minCookTime: 100,
-		maxCookTime: 120,
-		restaurant: 'Earthpure',
-		name: 'Earthpure Drinks',
-		imageUrl: 'https://source.unsplash.com/random/400x400?Drinks',
-	},
-	{
-		id: '628b5dec2abdabdbe19281e1',
-		index: 195,
-		rating: 4.1983,
-		promotion: null,
-		isNew: false,
-		categoryId: '6288a89f70dc8cf93b71609b',
-		minCookTime: 80,
-		maxCookTime: 100,
-		restaurant: 'Comveyer',
-		name: 'Comveyer Hot Meals',
-		imageUrl: 'https://source.unsplash.com/random/400x400?Hot%20Meals',
-	},
-];
+import { MOCK_FOODS } from '../../mocks/mockData';
 
 afterEach(() => {
 	cleanup();
 });
 
 describe('Food List Component', () => {
-	render(<FoodList list={data} />);
+	render(<FoodList list={MOCK_FOODS.foods as Food[]} />);
 	const foodList = screen.getByTestId('foods');
 
 	test('Component rendered', () => {
